bec4fa0809a3e9d099efa6a2ad5fabe1266f8d2d
lblum <luca.bluem95@gmail.com>
2022-02-22 15:04:07 +0100

    added f1 score and matthews correlation to evaluate CV

--- experiments/case_studies.py
+++ experiments/case_studies.py
@@ -10,6 +10,7 @@ from pandas import read_csv
 import sklearn
 from sklearn.datasets import load_breast_cancer
 from sklearn import clone
+from sklearn.metrics import make_scorer, matthews_corrcoef
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
@@ -22,6 +23,7 @@ from sklearn.svm import SVC
 from boxcox.optimization import Gridsearch2D
 from boxcox.optimization import ClassifierOptimization
 from boxcox.optimization import IterativeOptimizer
+from boxcox.optimization import MLEDiagonalOptimizer
 
 
 def show_data(features, labels, store_path=None):
@@ -43,19 +45,20 @@ def show_data(features, labels, store_path=None):
     return
 
 
-def evaluate_performance(features, labels, seed=42, optimizer=None):
+def evaluate_performance(features, labels, seed=42, optimizer=None, metric='accuracy'):
     """
     Evaluates performance for 5 different classifiers
     :param features: samples
     :param labels: labels of samples
     :param seed: seed for random number generator
     :param optimizer: procedure to find optimized lambda parameters for Box-Cox transformation
+    :param metric: which metric to use to evaluate classifier
     :return: performance of box cox transformaed data, not transformed data and the corresponding standard deviations
     """
     pipelines = {"linear": Pipeline(steps=[('classifier', SGDClassifier(loss="perceptron", random_state=seed))]),
                  "knn": Pipeline(steps=[('classifier', KNeighborsClassifier())]),
                  "bayesian": Pipeline(steps=[('classifier', GaussianNB())]),
-                 "SVM": Pipeline(steps=[('classifier', SVC(random_state=seed))]),
+                 "SVM": Pipeline(steps=[('classifier', SVC(random_state=seed, probability=True))]),
                  "NN": Pipeline(steps=[('classifier', MLPClassifier(hidden_layer_sizes=(10, 10), max_iter=1000,
                                                                     random_state=seed))])
                  }
@@ -68,7 +71,7 @@ def evaluate_performance(features, labels, seed=42, optimizer=None):
 
     for idx, (key, classifier) in enumerate(pipelines.items()):
         acc, std, acc_base, std_base = evaluate_classifier_performance(features, labels, classifier,
-                                                                       optimizer=optimizer)
+                                                                       optimizer=optimizer, metric=metric)
 
         print('Test accuracy for ' + key + ':  %.5f (%.5f)' % (float(acc), float(std)))
         print('Test accuracy for base ' + key + ':  %.5f (%.5f)' % (float(acc_base), float(std_base)))
@@ -83,7 +86,8 @@ def evaluate_performance(features, labels, seed=42, optimizer=None):
     return performance_box_cox, standard_dev_box_cox, performance_base, standard_dev_base
 
 
-def evaluate_classifier_performance(features, labels, classifier, seed=42, outer_cv=10, n_repeats=5, optimizer=None):
+def evaluate_classifier_performance(features, labels, classifier, seed=42, outer_cv=10, n_repeats=5, optimizer=None,
+                                    metric='accuracy'):
     """
     Evaluates the performance of the model with CV and optimizing for lambda
     :param features: samples
@@ -93,6 +97,7 @@ def evaluate_classifier_performance(features, labels, classifier, seed=42, outer
     :param outer_cv: number of folds for outer cross validation
     :param n_repeats: number of repeated cross validation runs
     :param optimizer: optimization procedure to get optimized lambda values
+    :param metric: which metric to use to evaluate cross validation
     :return: performance of box cox transformaed data, not transformed data and the corresponding standard deviations
     """
     classifier_temp = clone(classifier)
@@ -100,8 +105,16 @@ def evaluate_classifier_performance(features, labels, classifier, seed=42, outer
 
     features_temp = np.copy(features)
     cv = RepeatedStratifiedKFold(n_splits=outer_cv, n_repeats=n_repeats, random_state=seed)
-    n_scores = cross_val_score(pipeline, features_temp, labels, scoring='accuracy', cv=cv, n_jobs=-1,
-                               error_score='raise')
+
+    if metric in ['accuracy', 'f1']:
+        n_scores = cross_val_score(pipeline, features_temp, labels, scoring=metric, cv=cv, n_jobs=-1,
+                                   error_score='raise')
+    elif metric == 'matthews':
+        matthew_scorer = make_scorer(matthews_corrcoef)
+        n_scores = cross_val_score(pipeline, features_temp, labels, scoring=matthew_scorer, cv=cv, n_jobs=-1,
+                                   error_score='raise')
+    else:
+        exit("Metric is not supported. Available metrics are [accuracy, f1, matthews]")
 
     acc = np.mean(n_scores)
     std = np.std(n_scores) / np.sqrt(len(n_scores))
@@ -117,8 +130,17 @@ def evaluate_classifier_performance(features, labels, classifier, seed=42, outer
         pipeline = Pipeline(steps=[('optimize_lambda', ClassifierOptimization(classifier_temp, optimizer))])
 
     cv = RepeatedStratifiedKFold(n_splits=outer_cv, n_repeats=n_repeats, random_state=seed)
-    n_scores = cross_val_score(pipeline, features_temp, labels, scoring='accuracy', cv=cv, n_jobs=-1,
-                               error_score='raise')
+
+    if metric in ['accuracy', 'f1']:
+        n_scores = cross_val_score(pipeline, features_temp, labels, scoring=metric, cv=cv, n_jobs=-1,
+                                   error_score='raise')
+    elif metric == 'matthews':
+        matthew_scorer = make_scorer(matthews_corrcoef)
+        n_scores = cross_val_score(pipeline, features_temp, labels, scoring=matthew_scorer, cv=cv, n_jobs=-1,
+                                   error_score='raise')
+    else:
+        exit("Metric is not supported. Available metrics are [accuracy, f1, matthews]")
+
     print(n_scores)
     acc = np.mean(n_scores)
     std = np.std(n_scores) / np.sqrt(len(n_scores))
@@ -129,7 +151,7 @@ def evaluate_classifier_performance(features, labels, classifier, seed=42, outer
     return performance_box_cox, std_box_cox, performance_base, std_base
 
 
-def run_sonar(optimizer, grid):
+def run_sonar(optimizer, grid, metric):
     random.seed(42)
     np.random.seed(42)
 
@@ -155,7 +177,8 @@ def run_sonar(optimizer, grid):
         print(X)
 
         perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y,
-                                                                                                optimizer=optimizer)
+                                                                                                optimizer=optimizer,
+                                                                                                metric=metric)
 
         print("Iterative")
         print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -186,7 +209,8 @@ def run_sonar(optimizer, grid):
     print("Features 7 40 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -217,7 +241,8 @@ def run_sonar(optimizer, grid):
     print("Feature 1 47 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -248,7 +273,8 @@ def run_sonar(optimizer, grid):
     print("Feaure 10 44\n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -279,7 +305,8 @@ def run_sonar(optimizer, grid):
     print("Features 11 35 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -289,7 +316,7 @@ def run_sonar(optimizer, grid):
     print(re.sub('[][]', '', np.array2string((perf_box_cox - perf_base) * 100, precision=3, separator=' & ')))
 
 
-def run_breast(optimizer, grid):
+def run_breast(optimizer, grid, metric):
     random.seed(42)
     np.random.seed(42)
 
@@ -307,7 +334,8 @@ def run_breast(optimizer, grid):
         print(X)
 
         perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y,
-                                                                                                optimizer=optimizer)
+                                                                                                optimizer=optimizer,
+                                                                                                metric=metric)
 
         print("Iterative")
         print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -332,7 +360,8 @@ def run_breast(optimizer, grid):
     print("Features 1 5 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -357,7 +386,8 @@ def run_breast(optimizer, grid):
     print("Feature 4 26 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -382,7 +412,8 @@ def run_breast(optimizer, grid):
     print("Feaure 3 23 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -407,7 +438,8 @@ def run_breast(optimizer, grid):
     print("Features 13 22 \n")
     print(X)
 
-    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer)
+    perf_box_cox, standard_dev_box_cox, perf_base, standard_dev_base = evaluate_performance(X, y, optimizer=optimizer,
+                                                                                                metric=metric)
 
     print("Iterative")
     print(re.sub('[][]', '', np.array2string(perf_box_cox * 100, precision=3, separator=' & ')))
@@ -425,7 +457,10 @@ if __name__ == '__main__':
     # Required positional arguments
     parser.add_argument('dataset_arg', type=str, help='specify dataset')
     parser.add_argument('optimizer', type=int,
-                        help='specify if holdout should be use during optimization')
+                        help='specify which optimizer to use {0:iterative, 1:2DGridsearch (only for 2D datasets), '
+                             '2:mle}')
+    parser.add_argument('--metric', type=str, help='specify what metric to use to evaluate cross validation '
+                                                   '[accuracy, f1, matthews]')
 
     # Optional arguments
     parser.add_argument('--number_lambdas', type=int, required=False,
@@ -440,6 +475,12 @@ if __name__ == '__main__':
                         help='number of finer for gridsearch. Default = epochs')
 
     args = parser.parse_args()
+
+    metric = ''
+    if args.metric in ['accuracy', 'f1', 'matthews']:
+        metric = args.metric
+    else:
+        exit("Metric is not supported. Available metrics are [accuracy, f1, matthews]")
 
     number_lambdas = 11
     if args.number_lambdas is not None:
@@ -474,6 +515,7 @@ if __name__ == '__main__':
     gridsearch = False
     if args.optimizer == 0:
         print("Iterative")
+        print("metric = " + metric)
         print("number of lambdas = " + str(number_lambdas))
         print("epochs = " + str(epochs))
         print("shifts = " + str(shift))
@@ -488,14 +530,20 @@ if __name__ == '__main__':
 
     elif args.optimizer == 1:
         print("Gridsearch")
+        print("metric = " + metric)
+        print("number of lambdas = " + str(number_lambdas))
         opt = Gridsearch2D(nr_points=number_lambdas)
         gridsearch = True
+    elif args.optimizer == 2:
+        print("MLE optimizer")
+        print("metric = " + metric)
+        opt = MLEDiagonalOptimizer()
     else:
         exit("optimizer does not exist")
 
     if args.dataset_arg == "sonar":
-        run_sonar(opt, gridsearch)
+        run_sonar(opt, gridsearch, metric)
     elif args.dataset_arg == "breast":
-        run_breast(opt, gridsearch)
+        run_breast(opt, gridsearch, metric)
     else:
         exit("dataset does not exist")
